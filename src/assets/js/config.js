ed7c63863ae837701aa224df079048007191e9be
Laama-Haddad <120408156+Laama-Haddad@users.noreply.github.com>
2024-02-29 19:32:26 +0400

    init chat app

---
+++ src/assets/js/config.js
@@ -0,0 +1,6 @@
+export const avatarPrefix = "https://storage.zego.im/zim/example/web/assets/"
+
+export const appConfig = {
+  appID: 117188229, // AppID - type: number
+  appSign: "c68eb86f20c8f1b6aa4106f3fe54dfca2ddc24461657a999099500aef83e60cf", // AppSign - type: 64 byte length string
+}
